6768dec6a1f0094e47609f9d49609cb0fada5658
Nabin Katwal <nabinkatwal2000@gmail.com>
2025-01-27 12:26:50 +0545

    strings?

---
+++ arrays/string.py
@@ -0,0 +1,14 @@
+mystring = "Hello World"
+
+for letter in mystring:
+    print(letter)
+    
+print(len(mystring))
+
+print(mystring[0])
+
+print(mystring[-1])
+
+print(mystring[1:5])
+
+print(mystring * 3)
